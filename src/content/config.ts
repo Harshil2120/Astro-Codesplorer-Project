bdb19f84eefb4c5000efabeb70c0b63e44ec46aa
Harshil2120 <harshil212@icloud.com>
2024-09-23 22:50:19 -0400

    added latest post logic to home page with dummy data

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import {defineCollection, z} from "astro:content";
+
+const postsCollection = defineCollection({
+      schema:({image})=>z.object({
+        author: z.string(),
+        date:z.coerce.date(),
+        categories: z.array(z.string()).optional(),
+        image:image(),
+        title:z.string()
+      })
+});
+
+export const collections={
+    posts:postsCollection,
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,12 @@
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {},
+	theme: {
+		extend: {
+			screens: {
+				'md-lg': { min: '821px' } // Between md and lg
+			}
+		}
+	},
 	plugins: [require('@tailwindcss/typography')]
 };
