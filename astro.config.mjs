7baaecb3f9fa5686d5654fa0fff4f21ef3367aaf
Harshil2120 <harshil212@icloud.com>
2025-08-27 17:40:25 -0400

    made changes in dependency for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import cloudflare from '@astrojs/cloudflare';
-
 import tunnel from 'astro-tunnel';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), tunnel()],
-  output: 'server',
-  adapter: cloudflare()
+	integrations: [tailwind(), react(), tunnel()],
+	output: 'server',
+	adapter: cloudflare()
 });
